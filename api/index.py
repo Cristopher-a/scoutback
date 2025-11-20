e2750810caeed90861062f770f1207d54983a82f
Cristopher <crisesv4@gmail.com>
2025-11-19 23:12:02 -0600

    .

--- api/index.py
+++ api/index.py
@@ -1,5 +1,13 @@
 from flask import Flask, request, jsonify
+import cloudinary
+import cloudinary.uploader
 
+# Configura Cloudinary antes de usar
+cloudinary.config(
+    cloud_name="djlskhtyf",
+    api_key="596625527778167",
+    api_secret="zTTEjWP4apxgFF8a-kYNRTzT_XY"
+)
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +16,22 @@ def home():
 
 @app.route('/pits', methods=['POST'])
 def pits():
-    data = request.get_json()
+    body = request.get_json()
+    region= body.get("region")
+    traction_type= body.get("traction_type")
+    specialty= body.get("specialty")
+    team_number= body.get("team_number")
+    battery_number= body.get("battery_number")
+    robot_image= body.get("robot_image")
+    strategy_image= body.get("strategy_image")
+    strategy_comment= body.get("strategy_comment")
+    robot_url = None
+    strategy_url = None
+    if robot_image:
+        robot_url = cloudinary.uploader.upload(robot_image).get("secure_url")
+
+    if strategy_image:
+        strategy_url = cloudinary.uploader.upload(strategy_image).get("secure_url")
+
     # Process the data as needed
-    return jsonify({"message": "Pits data received", "data": data})
+    return jsonify({"message": "Pits data received", "data": body})
