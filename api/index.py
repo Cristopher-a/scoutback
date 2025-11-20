7f109c0ccc16dc6d1b5f028f63206d6187a3f961
Cristopher <crisesv4@gmail.com>
2025-11-19 23:13:33 -0600

    supabase

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify
 import cloudinary
 import cloudinary.uploader
+from supabase import create_client, Client
+
+SUPABASE_URL = "https://rhttqmtzcmwilzshnxwq.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJodHRxbXR6Y213aWx6c2hueHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1OTUwOTAsImV4cCI6MjA3OTE3MTA5MH0.8dYvM8CBEdqiF9ZZhaYRKhtOin_wYGf4JYrmTTIsX74"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Configura Cloudinary antes de usar
 cloudinary.config(
@@ -32,6 +38,17 @@ def pits():
 
     if strategy_image:
         strategy_url = cloudinary.uploader.upload(strategy_image).get("secure_url")
+    data= {
+        "region": region,
+        "traction_type": traction_type,
+        "specialty": specialty,
+        "team_number": team_number,
+        "battery_number": battery_number,
+        "robot_image": robot_url,
+        "strategy_image": strategy_url,
+        "strategy_comment": strategy_comment
+    }
 
+    response = supabase.table("pits").insert(data).execute()
     # Process the data as needed
     return jsonify({"message": "Pits data received", "data": body})
